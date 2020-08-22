68b331172d319898f7ab6232a583c080187cfa5f
otaviolube <otaviolube@gmail.com>
2020-08-22 17:03:32 -0300

    Webpack configurado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const soma = (x, y) => x + y;
+import { soma, multiplicacao } from './soma';
 
-console.log(soma(10,20));
+console.log(soma(20,30));
+
+console.log(multiplicacao(200,10));
---
+++ src/soma.js
@@ -0,0 +1,5 @@
+export const soma = (x,y) => {
+    return x+y
+};
+
+export const multiplicacao = (x,y) => x*y;
